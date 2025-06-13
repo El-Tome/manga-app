38d8a0e5ae01338776f63d4fcb4b4c38db21ffc8
Tom Chaumette <chaumettetom@gmail.com>
2025-06-13 23:18:34 +0200

    💄 Ajout style register

--- app/src/Form/RegistrationForm.php
+++ app/src/Form/RegistrationForm.php
@@ -5,9 +5,12 @@ namespace App\Form;
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Email;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
@@ -17,9 +20,45 @@ class RegistrationForm extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('firstName')
-            ->add('lastName')
+            ->add('email', EmailType::class, [
+                'label' => 'Email',
+                'attr' => [
+                    'class' => 'form-control',
+                    'placeholder' => 'Your email'
+                ],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'please enter your email',
+                    ]),
+                    new Email([
+                        'message' => 'Please enter a valid email',
+                    ])
+                ],
+            ])
+            ->add('firstName', TextType::class, [
+                'label' => 'First name',
+                'attr' => [
+                    'class' => 'form-control',
+                    'placeholder' => 'Your first name'
+                ],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'please enter your first name',
+                    ]),
+                ],
+            ])
+            ->add('lastName', TextType::class, [
+                'label' => 'Last name',
+                'attr' => [
+                    'class' => 'form-control',
+                    'placeholder' => 'Your last name'
+                ],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'please enter your last name',
+                    ]),
+                ]
+            ])
             ->add('agreeTerms', CheckboxType::class, [
                 'mapped' => false,
                 'constraints' => [
@@ -38,7 +77,7 @@ class RegistrationForm extends AbstractType
                         'message' => 'Please enter a password',
                     ]),
                     new Length([
-                        'min' => 6,
+                        'min' => 10,
                         'minMessage' => 'Your password should be at least {{ limit }} characters',
                         // max length allowed by Symfony for security reasons
                         'max' => 4096,
